dbb689fb284b8bae505bef42fd22d98979681fbb
Michala Lišková <michala.liskova@email.cz>
2022-04-18 22:13:32 +0200

    test metod s vytvořením dvou želv a jejich použití (není součástí úkolu ;-)  )

--- src/main/java/net/sevecek/turtle/HlavniProgram.java
+++ src/main/java/net/sevecek/turtle/HlavniProgram.java
@@ -5,9 +5,58 @@ import net.sevecek.turtle.engine.*;
 public class HlavniProgram {
 
     public void main(String[] args) {
-        Turtle zofka;
-
+        Turtle zofka;          //Turtle je třída objektů- je to nadefinované, co může dělat, a jméno želvy jsou ty objekty, které jsem si vyvolala
+        Turtle pepa;
         zofka = new Turtle();
+        pepa = new Turtle();
+        pepa.move(200);
+
+        nakresliPrasatko(zofka);
+        nakresliPrasatko(pepa);
+
+
     }
+
+    private void nakresliPrasatko(Turtle zelva) {
+        //tělo
+        zelva.move(100);
+        zelva.turnLeft(90);
+        zelva.move(170);
+        zelva.turnLeft(90);
+        zelva.move(100);
+        zelva.turnLeft(90);
+        zelva.move(170);
+        //zadní nohy
+        zelva.turnRight(60);
+        zelva.move(50);
+        zelva.penUp();
+        zelva.turnLeft(180);
+        zelva.move(50);
+        zelva.penDown();
+        zelva.turnLeft(120);
+        zelva.move(50);
+        zelva.turnRight(180);
+        zelva.move(50);
+        //přesun na přední nohy
+        zelva.turnLeft(120);
+        zelva.move(170);
+        //přední nohy
+        zelva.turnLeft(60);
+        zelva.move(50);
+        zelva.penUp();
+        zelva.turnRight(180);
+        zelva.move(50);
+        zelva.penDown();
+        zelva.turnRight(120);
+        zelva.move(50);
+        zelva.turnLeft(180);
+        zelva.move(50);
+        //hlavička
+        zelva.turnLeft(30);
+        zelva.move(100);
+        zelva.turnRight(120);
+        zelva.move(100);
+    }
+
 
 }
